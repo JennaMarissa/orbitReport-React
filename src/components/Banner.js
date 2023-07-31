9646d86ac4b35995efec4ffc1278b02c8279a546
JennaMarissa <31870484+JennaMarissa@users.noreply.github.com>
2023-07-31 08:20:43 -0500

    successfully added banner; making progress

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+const Banner = () => {
+    // should return header tag saying "Orbit Report"
+  return (
+    <div>
+        <header><h1>Orbit Report</h1></header>
+        <p>Click on the buttons to see the satellites in that orbit type</p>
+        </div>
+    );
+//  {/* below header create <p> tag with user instructions */}
+
+};
+
+export default Banner
