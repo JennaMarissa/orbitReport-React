1ce8a909f2e1adf97b04695041d009bc6e838ce8
JennaMarissa <31870484+JennaMarissa@users.noreply.github.com>
2023-07-31 21:26:15 -0500

    so close

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,31 @@
+import satData from "./satData";
+import Banner from "../App";
+import Table from "../App";
+import filterByType from "../App";
+import setSat from "../App";
+import displaySats from "../App";
+import sat from "../App";
+import orbitType from "../App";
+
 const Buttons = () => {
+<>
+  <Banner />
+  <Buttons
+    filterByType={filterByType}
+    setSat={setSat}
+    displaySats={displaySats}
+   />
+   <Table sat={sat} />
+</>
+displaySats.map((sat, id) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      <button onClick={() => filterByType(sat)} key={id}>{sat} Orbit</button>
+  
       </div>
   );
-};
+})}
+<button onClick={() => setSat(satData)} key={orbitType}>All Orbits</button>
+
 
 export default Buttons;
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,18 +1,30 @@
+// import sat from "./satData";
+
 const Table = ({ sat }) => {
   return (
       <table>
        <thead>
         <tr>
-          <th>Header TBD</th>
+          <th>Name</th>
+          <th>Type</th>
+          <th>LaunchDate</th>
+          <th>Status</th>
         </tr>
         </thead>
         <tbody>
-        <tr>
-          <td>Row Data TBD</td>
-        </tr>
+        {sat.map((data, id) => {
+            return (
+              <tr key ={id}>
+                <td>{data.name}</td>
+                <td>{data.type}</td>
+                <td>{data.launchDate}</td>
+                <td>{data.operational}</td>
+              </tr>
+            )  
+        } ) }
         </tbody>
       </table>
   );
 };
-
+// console.log({sat});
 export default Table;
